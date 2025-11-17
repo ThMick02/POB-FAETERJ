72c08733b804b04cf9cfe4130b6bd4d1209f6fe5
Thiago <thiagotavares0202@gmail.com>
2025-11-17 15:52:18 -0300

    Update Funcionario.java
    Correções de redundancia.

--- Funcionario.java
+++ Funcionario.java
@@ -3,22 +3,15 @@ package T4;
 public class Funcionario {
     private static int proxMatricula=0;
     private String nome;
-    private int matricula;
+    private final int matricula;
     private double salario;
 
     public Funcionario(String n, double s) throws IllegalArgumentException, ViolacaoCltException {
-        if (n.length()>40) {
-            throw new IllegalArgumentException ("Requisição negada: o nome '" + n + "' excede o limite de 40 caracteres.");
-        }
-        else if (s<350) {
-            throw new ViolacaoCltException ("Requisição negada: valor do salario atribuído a " + n + " violam os direitos garantidos na Clt pelo Art. ", 76);
-        }
-        else {
+        setNome(n);
+        setSalario(s);
         this.matricula=proxMatricula;
         proxMatricula++;
-        this.nome=n;
-        this.salario=s;
-        }
+        
     }
 
     public String getNome() {
@@ -31,11 +24,23 @@ public class Funcionario {
         return this.matricula;
     }
 
-    public void setSalario(double s) throws AumentoAbsurdoException {
+    public void setNome(String n) throws IllegalArgumentException {
+        if (n.length()>40) {
+            throw new IllegalArgumentException("Requisição negada: o nome '" + n + "' excede o limite de 40 caracteres.");
+        }
+        this.nome=n;
         
+    }
+
+    public void setSalario(double s) throws AumentoAbsurdoException, ViolacaoCltException {
         double salarioMaximo = salario*1.10;
-        if (s>=salarioMaximo) {
-            throw new AumentoAbsurdoException ("Requisição de aumento para o funcionário " + nome + " foi negada: valor indevido.");
+        if (s<350) {
+            throw new ViolacaoCltException ("Requisição negada: valor do salario atribuído a " + nome + " violam os direitos garantidos na Clt pelo Art. ", 76);
+        }
+        if (salario!=0) {
+            if (s>=salarioMaximo) {
+                throw new AumentoAbsurdoException ("Requisição de aumento para o funcionário " + nome + " foi negada: valor indevido.");
+            }
         }
         this.salario=s;
     }
